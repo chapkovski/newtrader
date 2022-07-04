7e14fa0ca48deccd1a0e1f79f6216b0c575f8710
Philipp Chapkovski <chapkovski@gmail.com>
2022-07-04 11:46:51 +0200

    upd trading_at default

--- settings.py
+++ settings.py
@@ -66,7 +66,7 @@ SESSION_CONFIG_DEFAULTS = dict(
     for_prolific=False,
     prolific_redirect_url='http://www.lenta.ru',
     prediction_at=10,
-    trading_at=0,
+    trading_at=6,
     tick_frequency=6,
 )
 
