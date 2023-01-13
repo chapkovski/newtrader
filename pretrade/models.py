f71947b3ab809ec5017249fc1c89f852f72d27c9
Marius Zoican <m.a.zoican@gmail.com>
2023-01-13 15:52:03 -0500

    R79
    Replaced "More likely" with "likelier" as per referee suggestion. Updated CQ to read as if we have a single stock and deleted the question about price notifications in one stock only.

--- pretrade/models.py
+++ pretrade/models.py
@@ -40,10 +40,10 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     cq1 = models.StringField(
-        label="The price of Stock A just went up. At the next price update,",
-        choices=["Stock A is more likely to go up again",
-                 "Stock A is more likely to go down",
-                 "Stock A is equally likely to go up or down",
+        label="The stock price just went up. At the next price update,",
+        choices=["The stock price is likelier to go up again",
+                 "The stock price is likelier to go down",
+                 "The stock price is equally likely to go up or down",
                  ],
         widget=widgets.RadioSelect
     )
@@ -57,15 +57,15 @@ class Player(BasePlayer):
         widget=widgets.RadioSelect
     )
 
-    cq3 = models.StringField(
-        label="Price change notifications are enabled:",
-        choices=["For both stocks, in all rounds",
-                 "For stock A only, in some rounds",
-                 "For stock A only, in all rounds",
-                 "For stock B only, in some rounds"
-                 ],
-        widget=widgets.RadioSelect
-    )
+#     cq3 = models.StringField(
+#         label="Price change notifications are enabled:",
+#         choices=["For both stocks, in all rounds",
+#                  "For stock A only, in some rounds",
+#                  "For stock A only, in all rounds",
+#                  "For stock B only, in some rounds"
+#                  ],
+#         widget=widgets.RadioSelect
+#     )
 
     cq4 = models.StringField(
         label="Your total bonus payment for the experiment depends on:",
@@ -78,29 +78,29 @@ class Player(BasePlayer):
 
     cq5 = models.StringField(
         label="When is the trade count updated?",
-        choices=["If your position in any stock changes between two consecutive price updates",
-                 "If your position in both stocks changes between two consecutive price updates.",
+        choices=["If your position in the stock changes between two consecutive price updates",
+                 "If your position in the stock increases between two consecutive price updates.",
                  "Every time you click the BUY or SELL buttons, even between two consecutive price updates"
                  ],
         widget=widgets.RadioSelect
     )
 
     def cq1_error_message(self, value):
-        if value != "Stock A is more likely to go up again":
+        if value != "The stock price is likelier to go up again":
             return 'Wrong answer!'
 
     def cq2_error_message(self, value):
         if value != "You can purchase it, but any negative cash balance is subtracted from your final earnings":
             return 'Wrong answer!'
 
-    def cq3_error_message(self, value):
-        if value != "For stock A only, in some rounds":
-            return 'Wrong answer!'
+#     def cq3_error_message(self, value):
+#         if value != "For stock A only, in some rounds":
+#             return 'Wrong answer!'
 
     def cq4_error_message(self, value):
         if value != 'Your payoff in a randomly selected round and your correct answers in the post-experimental quiz':
             return 'Wrong answer!'
 
     def cq5_error_message(self, value):
-        if value != "If your position in any stock changes between two consecutive price updates":
+        if value != "If your position in the stock changes between two consecutive price updates":
             return 'Wrong answer!'
